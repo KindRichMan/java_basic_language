75da058de88cef67e3b2706d1915727ff0b522e0
kindrichmuhwan <anghks12@naver.com>
2023-04-06 15:29:37 +0900

    my first commit

---
+++ Variables.java
@@ -0,0 +1,30 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Variables {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        // Press Shiccft+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+             // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+            System.out.println("abc");
+            //sout
+        }
+        System.out.println("abc");
+
+
+        int fee = 0;
+        String whois = null;
+        if(whois.equals("student")){
+            fee = 500;
+            System.out.println("학생의 요금은 "+fee +"입니다.");
+        }else{
+            System.out.println("성인의 요금은 1000원입니다. ");
+        }
+    }
+}
